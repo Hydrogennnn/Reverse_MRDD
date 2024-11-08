472fcae8f64f6fc47b48ba9e128c61412cc68e37
wjd <2902704036@qq.com>
2024-11-08 23:09:17 +0800

    upd wandb config

--- train_distent.py
+++ train_distent.py
@@ -165,7 +165,7 @@ if __name__ == '__main__':
     if use_wandb:
         wandb.init(project=config.project_name,
                 config=config,
-                name=f'{config.experiment_name}-rmrdd-c{config.consistency.c_dim}--v{config.vspecific.v_dim}-m{config.train.masked_ratio}-mv{config.train.mask_view_ratio if config.train.mask_view else 0.0}-{"modal missing" if config.train.mask_view else "full modal"}-{seed}')
+                name=f'{config.experiment_name}-rmrdd-c{config.consistency.c_dim}--v{config.vspecific.v_dim}-m{config.train.masked_ratio}-mv{config.train.mask_view_ratio if config.train.mask_view else 0.0}-{"modal missing" if config.train.val_mask_view else "full modal"}-{seed}')
     summary(model)
     smartprint('model loaded!')
     if LOCAL_RANK == 0 or LOCAL_RANK == -1:
